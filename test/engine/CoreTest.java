510eddc9fea746f869f702028595fc28937c192c
farhiya11 <fayus20@student.sdu.dk>
2022-12-06 22:26:35 +0900

    Enemy disappears and reapears

---
+++ test/engine/CoreTest.java
@@ -0,0 +1,48 @@
+package engine;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoreTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void getLogger() {
+    }
+
+    @Test
+    void getDrawManager() {
+    }
+
+    @Test
+    void getInputManager() {
+    }
+
+    @Test
+    void getFileManager() {
+    }
+
+    @Test
+    void getCooldown() {
+    }
+
+    @Test
+    void getVariableCooldown() {
+    }
+
+    @Test
+    void setSize() {
+    }
+
+    @Test
+    void getDiff() {
+    }
+
+    @Test
+    void getWIDTH() {
+    }
+}
